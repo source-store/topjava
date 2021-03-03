0595d02325f7338d45e7a4beac6c8a58cb8033d2
source-store <73580046+source-store@users.noreply.github.com>
2021-03-03 16:59:58 +0300

    fix8

--- src/test/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
+++ src/test/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
@@ -9,8 +9,6 @@ import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 import ru.javawebinar.topjava.util.Util;
 
-//import javax.annotation.PostConstruct;
-//import javax.annotation.PreDestroy;
 import java.time.LocalDateTime;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
@@ -37,16 +35,6 @@ public class InMemoryMealRepository implements MealRepository {
         InMemoryBaseRepository<Meal> meals = usersMealsMap.computeIfAbsent(userId, uid -> new InMemoryBaseRepository<>());
         return meals.save(meal);
     }
-
-//    @PostConstruct
-//    public void postConstruct() {
-//        log.info("+++ PostConstruct");
-//    }
-
-//    @PreDestroy
-//    public void preDestroy() {
-//        log.info("+++ PreDestroy");
-//    }
 
     @Override
     public boolean delete(int id, int userId) {
